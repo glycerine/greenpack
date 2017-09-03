f679d9fb6f1c3ab0f724275c6c798c4d933b192c
Jason E. Aten <j.e.aten@gmail.com>
2017-09-03 11:17:01 -0500

    ft. failing test _generated/slciface_test.go

---
+++ _generated/slciface_test.go
@@ -0,0 +1,28 @@
+package _generated
+
+import (
+	"testing"
+)
+
+//go:generate greenpack
+
+type Row struct {
+	K []interface{} `msg:"K"`
+	V []interface{} `msg:"V"`
+	T int64         `msg:"T"`
+}
+
+// https://github.com/glycerine/greenpack/issues/1
+func TestMarshalSliceOfInterface(t *testing.T) {
+
+	m := make(map[string][]float64)
+	m["def"] = []float64{1.2, 3.4}
+
+	row := Row{}
+	row.K = []interface{}{"abc"}
+	row.V = []interface{}{m}
+	_, err := row.MarshalMsg(nil)
+	if err != nil {
+		panic(err) // panic: msgp: type "map[string][]float64" not supported
+	}
+}
