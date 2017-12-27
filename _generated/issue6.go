a5142b384c9e3ebde094879040c0e44f429f54cd
Jason E. Aten <j.e.aten@gmail.com>
2017-12-26 20:36:38 -0600

    atg. restore the issue6 test to in force

--- _generated/issue6.go
+++ _generated/issue6.go
@@ -10,10 +10,10 @@ var _ = time.Time{}
 
 // greenpack Issue 6: must be big enough to *NOT* inline.
 type Event struct {
-	ID          []byte `zid:"0",msg:"id"`
-	AggregateID []byte `zid:"1",msg:"aggregate_id"`
-	Type        string `zid:"2",msg:"type"`
-	//	Time        time.Time `zid:"3",msg:"time"`
+	ID          []byte    `zid:"0",msg:"id"`
+	AggregateID []byte    `zid:"1",msg:"aggregate_id"`
+	Type        string    `zid:"2",msg:"type"`
+	Time        time.Time `zid:"3",msg:"time"`
 }
 
 type Commit struct {
